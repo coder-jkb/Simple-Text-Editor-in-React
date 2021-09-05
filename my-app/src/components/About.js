08100c5de74aee0aa9351bd06d382ab427bdc9f7
coder-jkb <jkb17codes@gmail.com>
2021-09-05 20:02:51 +0530

    About page edited

--- my-app/src/components/About.js
+++ my-app/src/components/About.js
@@ -3,9 +3,10 @@ import React from 'react'
 export default function About(props) {
     return (
         <div className={"container text-"+props.mode} >
-            <h1 className="about">About Me</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet tempora omnis officiis consectetur velit accusantium aspernatur odit, dolorum libero perspiciatis optio reiciendis corporis sequi quas nobis rerum similique, architecto vitae ullam facilis ex iusto atque.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet tempora omnis officiis consectetur velit accusa aspernatur odit, dolorum libero perspiciatis optio reiciendis corporis sequi quas nobis rerum similique, architecto vitae ullam facilis ex iusto atque.</p>
+            <h1 className="about">About</h1>
+            I am <strong>Jay Bhanushali</strong> I have built this simple text utility application as a project while learning react from <a href="https://www.youtube.com/watch?v=-mJFZp84TIY&list=PLu0W_9lII9agx66oZnT6IyhcMIbUMNMdt" target="_blank" rel="noreferrer"><strong>this</strong></a> youtube tutorial by <a href="https://www.youtube.com/c/CodeWithHarry" target="_blank" rel="noreferrer"><strong>CodeWithHarry</strong></a>
+            I like to do web developement especially backend. I am also learning Machine Learning...
+            I know bacis of C/C++, Python, HTML/CSS/JS and currently learning react :D. A pull request for any improvement in thhis project is welcomed. You can have a look at the repo by visiting <a href="https://git.io/Jutb8" target="_blank" rel="noreferrer"> here Simple-Text-Editor-in-React</a>
         </div>
     )
 }
